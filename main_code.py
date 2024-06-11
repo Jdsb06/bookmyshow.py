28ef9abe1b9a7ffaae3cd49fe3ffc2e71e7ce947
Jdsb06 <107495919+Jdsb06@users.noreply.github.com>
2024-06-11 17:17:01 +0530

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -5,7 +5,7 @@ import platform
 
 conn = pymysql.connect(host="localhost",
                                user="root",
-                               password="rb",
+                               password="ADD YOUR PASSWORD",
                                database="bookmyshow")
 cursor = conn.cursor()
 
@@ -446,7 +446,7 @@ def cancel_booking(cursor, user_id):
 def modify_view_account():
     conn = pymysql.connect(host="localhost",
                                    user="root",
-                                   password="rb",
+                                   password="ADD YOUR PASSWORD",
                                    database="bookmyshow")
     global current_user_id
     global ch, opp
